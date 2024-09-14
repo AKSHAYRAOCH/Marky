9f67946a424696e1225d54272455e96a982bea63
akshayraoch <chakshayrao@gmail.com>
2024-09-14 19:06:33 +0530

    "removed annotation"

--- backend/src/db/db.ts
+++ backend/src/db/db.ts
@@ -3,7 +3,7 @@ import { drizzle } from "drizzle-orm/node-postgres";
 import { Pool } from "pg";
 
 const pool = new Pool({
-  connectionString: process.env.POSTGRES_URL!,
+  connectionString: process.env.POSTGRES_URL,
 });
 
 
